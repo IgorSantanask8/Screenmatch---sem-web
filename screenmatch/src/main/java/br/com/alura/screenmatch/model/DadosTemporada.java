a50c375b537221d5199e19db104222de51dc9fd9
IgorSantanask8 <santanaigor60@outlook.com>
2025-12-31 19:02:53 -0300

    Evoluindo screenmacth

--- screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosTemporada.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosTemporada.java
@@ -3,8 +3,9 @@ package br.com.alura.screenmatch.models;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DadoSerie(@JsonAlias("Title")String titulo,
-                        @JsonAlias("totalSeasons")Integer totalTemporadas,
-                        @JsonAlias("imdbRating")String avalicao) {
+public record DadosTemporada(@JsonAlias("Season") Integer temporada,
+                             @JsonAlias("Episodes") List<DadosEpisodios> episodios) {
 }
